3f32512dd3ed7050d28816e452c2db3656839ad8
alban.leau <alban.leau@gmail.com>
2019-11-26 22:13:19 +0100

    Add @angular/material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { CityListComponent } from './components/city-list/city-list.component';
+import { CityDetailComponent } from './components/city-detail/city-detail.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule, MatPaginatorModule, MatProgressSpinnerModule, 
+  MatSortModule, MatTableModule, MatIconModule, MatButtonModule } from '@angular/material';
+import { CityService } from './services/city.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CityListComponent,
+    CityDetailComponent,
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatInputModule,
+    MatPaginatorModule, 
+    MatProgressSpinnerModule, 
+    MatSortModule,
+    MatTableModule,
+    MatIconModule,
+    MatButtonModule,
   ],
-  providers: [],
+  providers: [
+    CityService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
